e70457f759ae9bc3cd8cf5c011b8048f09c2882e
Jay Agrawal <jay7697agrawal@gmail.com>
2021-06-28 15:19:19 +0530

    nodejs project first commit

---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+const cityName = document.getElementById("cityName");
+const submitBtn = document.getElementById("submitBtn");
+const city_name = document.getElementById("city_name");
+const temp_status = document.getElementById("temp_status");
+const temp_real = document.getElementById("temp_real");
+const datahide = document.querySelector(".middle_layer");
+
+const getInfo = async(event) => {
+    event.preventDefault();
+    let cityVal = cityName.value;
+    if(cityVal==="") {
+        city_name.innerText = "Please write the city before searching....";
+        datahide.classList.add('data_hide');
+    }
+    else{
+        try{
+            const appid="appid=4998eb4205ec0c0fb45c37ed33ea1fdb";
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&${appid}&units=metric`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData = [data];
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real.innerText = arrData[0].main.temp;
+            temp_status.innerText = arrData[0].weather[0].main;
+
+            const tempMood = arrData[0].weather[0].main;
+
+            if(tempMood == "Clear")
+                temp_status.innerHTML = "<i class = 'fas fa-sun' style = 'color: #eccc68'></i>";
+            else if(tempMood=="Clouds")
+            temp_status.innerHTML = "<i class = 'fas fa-cloud' style = 'color: #f1f2f6'></i>";
+            else if(tempMood=="Rain")
+            temp_status.innerHTML = "<i class = 'fas fa-cloud-rain' style = 'color: #a4b0be'></i>";
+            else
+            temp_status.innerHTML = "<i class = 'fas fa-sun' style = 'color: #eccc68'></i>"
+            datahide.classList.remove('data_hide');
+        }
+
+        catch{
+            city_name.innerText = "Please write the city correctly....";
+            console.log(cityVal)
+            datahide.classList.add('data_hide');;
+        }
+    }
+
+}
+submitBtn.addEventListener("click",getInfo);
